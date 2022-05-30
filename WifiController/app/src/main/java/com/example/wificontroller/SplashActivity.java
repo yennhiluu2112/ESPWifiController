896969848ff892faf9fcf34ea8a6ae5897395cbb
yennhiluu2112 <yennhiluu2112@gmail.com>
2022-05-30 21:24:15 +0700

    nhi cm splash

---
+++ WifiController/app/src/main/java/com/example/wificontroller/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.wificontroller;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.wificontroller.databinding.ActivitySplashBinding;
+import com.github.ybq.android.spinkit.style.ChasingDots;
+import com.github.ybq.android.spinkit.style.Wave;
+
+public class SplashActivity extends AppCompatActivity {
+    ActivitySplashBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.splashProgress.setIndeterminateDrawableTiled(new Wave());
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        },3000);
+    }
+}
