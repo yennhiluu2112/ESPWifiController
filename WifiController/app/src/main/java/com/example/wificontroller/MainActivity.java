032a7a5c1e3f0fe64d8097a079618c4b467c351f
yennhiluu2112 <yennhiluu2112@gmail.com>
2022-05-30 23:36:00 +0700

    nhi cm main

--- WifiController/app/src/main/java/com/example/wificontroller/MainActivity.java
+++ WifiController/app/src/main/java/com/example/wificontroller/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.wificontroller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.wificontroller.databinding.ActivityMainBinding;
+import com.example.wificontroller.databinding.ActivitySplashBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
     }
 }
